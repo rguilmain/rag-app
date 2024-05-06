0f8982badb447054ae78d2d7e8218002342e413c
Rich Guilmain <richguilmain@gmail.com>
2024-05-05 22:46:31 -0600

    simplify search endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 
 from app.search import search
@@ -9,15 +8,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello!"}
 
 
-@app.get("/search")
-def read_item(q: Union[str, None] = None):
-    response = {}
-    if q:
-        response.update({"answer": search(q)})
-    else:
-        response.update({"error": "No query provided"})
-    
-    return response
+@app.post("/search")
+async def search(text: str):
+    return {"response": search(text)}
