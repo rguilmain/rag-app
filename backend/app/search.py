badb310bfa771ebb29ea10a3b38782449a9f83ef
Rich Guilmain <richguilmain@gmail.com>
2024-05-17 21:55:57 -0600

    validate that api keys are set

--- backend/app/search.py
+++ backend/app/search.py
@@ -8,6 +8,13 @@ from langchain_openai import ChatOpenAI
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+
+if openai_api_key is None or openai_api_key == "":
+    raise ValueError("OPENAI_API_KEY is not set or empty")
+
+if tavily_api_key is None or tavily_api_key == "":
+    raise ValueError("TAVILY_API_KEY is not set or empty")
 
 retriever = TavilySearchAPIRetriever(k=3)
 
