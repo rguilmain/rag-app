db3a53f77a6fc625fcb2ec4fc9c107ba280e89a7
Rich Guilmain <richguilmain@gmail.com>
2024-05-25 05:23:06 -0600

    raise status code 400 if required param q not provided

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.search import perform_search
@@ -22,6 +22,9 @@ def read_root():
 @app.get("/search")
 async def search(q: str):
     try:
+        if not q:
+            raise HTTPException(status_code=400,
+                                detail="Query parameter `q` is required")
         answer = await perform_search(q)
         return {"answer": answer}
     except Exception as e:
