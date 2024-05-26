67e099fa05597788014d6d63d7729c93e7d56339
Rich Guilmain <richguilmain@gmail.com>
2024-05-26 07:11:14 -0600

    add a couple more example threads

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -11,14 +11,24 @@ function Sidebar() {
   return (
     <Drawer variant="permanent" anchor="left">
       <List>
-        {/* Loop through chat rooms or contacts */}
         <ListItem>
           <ListItemIcon>
             <ChatIcon />
           </ListItemIcon>
           <ListItemText primary="Thread 1" />
         </ListItem>
-        {/* Repeat for other chat rooms or contacts */}
+        <ListItem>
+          <ListItemIcon>
+            <ChatIcon />
+          </ListItemIcon>
+          <ListItemText primary="Thread 2" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <ChatIcon />
+          </ListItemIcon>
+          <ListItemText primary="Thread 3" />
+        </ListItem>
       </List>
     </Drawer>
   );
